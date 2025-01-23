2573da7c5ec8af0db42e960a05347be3a55a1e6f
SStoyanov22 <SStoyanov22@gmail.com>
2025-01-23 23:43:21 +0200

    Adding 2024/ day 6

--- years/2024/06/index.ts
+++ years/2024/06/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import { Cell, Grid } from "../../../util/grid";
 
 const YEAR = 2024;
 const DAY = 6;
@@ -13,12 +14,99 @@ const DAY = 6;
 // data path    : /Users/user/src/github.com/SStoyanov22/advent-of-code/years/2024/06/data.txt
 // problem url  : https://adventofcode.com/2024/day/6
 
+
 async function p2024day6_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const matrix = input.trim().split('\n').map((row) => row.split(''));
+
+	const patrolRow = matrix.findIndex((row) => row.includes('^'));
+	const patrolCol = matrix[patrolRow].indexOf('^');
+
+	const directions = [
+		[-1, 0],
+		[0, 1],
+		[1, 0],
+		[0, -1],
+	];
+
+	let dirIndex = 0;
+	let pos = [patrolRow, patrolCol];
+	const visited = new Set([JSON.stringify(pos)]);
+	while (true) {
+		const [row, col] = pos;
+		const [dRow, dCol] = directions[dirIndex];
+		const nextPos = [row + dRow, col + dCol];
+	
+		if (
+			nextPos[0] < 0 || nextPos[0] >= matrix.length ||
+			nextPos[1] < 0 || nextPos[1] >= matrix[0].length
+		) break;
+	
+		if (matrix[nextPos[0]][nextPos[1]] === '#') {
+			dirIndex = (dirIndex + 1) % directions.length;
+			continue;
+		}
+	
+		pos = nextPos;
+	
+		visited.add(JSON.stringify(pos));
+	}
+
+	return visited.size;
 }
 
 async function p2024day6_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const matrix = input.trim().split('\n').map((row) => row.split(''));
+
+	const patrolRow = matrix.findIndex((row) => row.includes('^'));
+	const patrolCol = matrix[patrolRow].indexOf('^');
+
+	const directions = [
+		[-1, 0],
+		[0, 1],
+		[1, 0],
+		[0, -1],
+	];
+	let obstructionCount = 0;
+
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (!['#', '^'].includes(matrix[i][j])) {
+				let dirIndex = 0;
+				let pos = [patrolRow, patrolCol];
+				const visited = new Set([
+					JSON.stringify([...pos, ...directions[dirIndex]]),
+				]);
+				const modifiedMatrix = matrix.map((row) => [...row]);
+				modifiedMatrix[i][j] = '#';
+
+				while (true) {
+					const [row, col] = pos;
+					const [dRow, dCol] = directions[dirIndex];
+					const nextPos = [row + dRow, col + dCol];
+
+					if (
+						nextPos[0] < 0 || nextPos[0] >= modifiedMatrix.length ||
+						nextPos[1] < 0 || nextPos[1] >= modifiedMatrix[0].length
+					) break;
+
+					if (modifiedMatrix[nextPos[0]][nextPos[1]] === '#') {
+						dirIndex = (dirIndex + 1) % directions.length;
+						continue;
+					}
+
+					pos = nextPos;
+
+					if (visited.has(JSON.stringify([...pos, ...directions[dirIndex]]))) {
+						obstructionCount += 1;
+						break;
+					}
+
+					visited.add(JSON.stringify([...pos, ...directions[dirIndex]]));
+				}
+			}
+		}
+	}
+	return obstructionCount
 }
 
 async function run() {
